b18b133ec14d83cef7a7c58ce9048bce069d0528
RXS <117028666+RXS27@users.noreply.github.com>
2025-04-09 21:25:41 +0900

    YOLO よろ！

---
+++ .history/test_20250409212201.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# モデルファイルを指定 (yolo11n.pt など)
+model = YOLO("yolo11n.pt")
+
+# 画像ファイル (bus.jpg) に対して物体検出を実行
+results = model.predict("bus.jpg", save=True, imgsz=320, conf=0.5)
+
+# これで runs/detect/predict フォルダに推論結果が保存される
